219cbfc057b8b27075a71ab4e3454628ac48591a
cyberviking5 <aviral.hatwal@gmail.com>
2023-06-20 16:50:08 +0530

    Adding signup form with regex and a little bit of change in routing.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Mainpage from './pages/main/Mainpage'
+import Signup from './pages/signup/Signup'
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Mainpage/>}/>
+        <Route path="/" element={<Signup/>}/>
+        <Route path="/home" element={<Mainpage/>}/>
       </Routes>
     </BrowserRouter>
   )
