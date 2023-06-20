72ba420f6854201b438d56745e1ce82c75351b97
cyberviking5 <aviral.hatwal@gmail.com>
2023-06-20 23:29:25 +0530

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Mainpage from './pages/main/Mainpage'
-<<<<<<< HEAD
 import Packages from './components/packageshotel/Packages'
-=======
 import Signup from './pages/signup/Signup'
 import Contactus from './pages/ContactUs/ContactUs'
->>>>>>> 68eb7122848038d1e826e5eba35f1b6697a0ac5c
 const App = () => {
   return (
     <BrowserRouter>
