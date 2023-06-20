ba716780cd8f6adc8d46a320ceaa414bf0d7f1c3
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-06-20 23:47:14 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Mainpage from './pages/main/Mainpage'
-<<<<<<< HEAD
 import Packages from './components/packageshotel/Packages'
-=======
 import Signup from './pages/signup/Signup'
 import Contactus from './pages/ContactUs/ContactUs'
->>>>>>> 68eb7122848038d1e826e5eba35f1b6697a0ac5c
 const App = () => {
   return (
     <BrowserRouter>
@@ -14,8 +11,6 @@ const App = () => {
         <Route path="/" element={<Signup/>}/>
         <Route path="/home" element={<Mainpage/>}/>
         <Route path="/contactus" element={<Contactus/>}/>
-      </Routes>
-      <Routes>
         <Route path="/packages" element={<Packages/>}/>
       </Routes>
     </BrowserRouter>
