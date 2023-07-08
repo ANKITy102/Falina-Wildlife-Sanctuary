d97ba4a7e1e3828b116dc87b14da7fe08b42cf02
Ankit Yadav <119873198+ANKITy102@users.noreply.github.com>
2023-07-08 13:42:06 +0530

    uploading MyVariables.java

---
+++ backend/backend/src/main/java/com/webdproject/backend/users/models/MyVariables.java
@@ -0,0 +1,28 @@
+package com.webdproject.backend.users.models;
+
+public class MyVariables {
+    private String client_id;
+    private String email;
+
+    public MyVariables() {
+        client_id = "163953454111-bb8n07nucej8pue99g24kcfu4120ltc2.apps.googleusercontent.com";
+        email = "mahesh9426346889@gmail.com";
+    }
+
+    public String getClient_id() {
+        return client_id;
+    }
+
+    public void setClient_id(String client_id) {
+        this.client_id = client_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
